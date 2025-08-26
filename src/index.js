a5ed8d292c49e69b4c9e52ffb8301b07db79d414
Teemunl <teemu.liimatta@hotmail.com>
2025-08-26 16:22:54 +0300

    port

--- src/index.js
+++ src/index.js
@@ -32,6 +32,6 @@ app.use('/', fileRoutes);
  * Palvelimen käynnistys
  * Kuuntelee määriteltyä porttia ja tulostaa viestin kun palvelin on valmis
  */
-app.listen(80, '0.0.0.0', () => {
-  console.log('Server running on port 80');
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
 });
