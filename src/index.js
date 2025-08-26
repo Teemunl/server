e8c1bd9b3355d58c328c7d2c69e5065fd7d6941a
Teemunl <teemu.liimatta@hotmail.com>
2025-08-26 18:10:42 +0300

    tst

--- src/index.js
+++ src/index.js
@@ -14,7 +14,7 @@ app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(fileUpload());
 
-app.use(express.static('public'));
+app.use(express.static(path.join(__dirname, '../public')));
 
 /**
  * Näyttömoottorin konfiguraatio
